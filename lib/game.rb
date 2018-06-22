1907d8012f74b86597eb534409eefc72bc90c047
m-pitera <mpitera483@gmail.com>
2018-06-22 14:41:45 -0500

    organized into seperate classes

--- lib/game.rb
+++ lib/game.rb
@@ -1,28 +1,23 @@
 # no structure yet, just drafting some logic
 # will split up into dif methods in the future
 
-class Game
-  attr_reader :guess, :code, :guesses_left, :first_time
+class User
+  attr_reader :guess, :guesses_left, :first_time
 
   def initialize
     @guesses_left = 10
-    @code = gen_secret_code
     @guess = ''
     @first_time = true
   end
 
-  # This is just testing methods and etc
-  # most of this was used for debugging and etc
-
   def introduce_user
     system 'clear'
-    title1 = yellow_b('*******************************')
-    title2 = yellow_b('*****') + red_b('Welcome to Mastermind') + yellow_b('*****')
-    title3 = yellow_b('*******************************')
+    stars = Colors.yellow_b('*******************************')
+    title = Colors.yellow_b('*****') + Colors.red_b('Welcome to Mastermind') + Colors.yellow_b('*****')
     puts('')
-    puts(title1.center(85))
-    puts(title2.center(103))
-    puts(title3.center(85))
+    puts(stars.center(85))
+    puts(title.center(103))
+    puts(stars.center(85))
     puts("""
       The rules are simple, the Computer will generate a secret code
     that you will have to guess. There are 6 colors with the code containing
@@ -60,8 +55,8 @@ class Game
   end
 
   def take_input
-    puts("\nYou have " + cyan(guesses_left) + ' guesses left')
-    print("What is your guess?\n\t" + purple_b('>>>'))
+    puts("\nYou have " + Colors.cyan(guesses_left) + ' guesses left')
+    print("What is your guess?\n\t" + Colors.purple_b('>>>'))
     the_input = gets.split(' ')
     # print("#{the_input}\n")
 
@@ -70,53 +65,23 @@ class Game
       @guesses_left -= 1
     else
       # further into dev I might check and return for specific cases of incorrect input
-      puts(red_b("Sorry, that's not the correct input, please refer to the instruction
+      puts(Colors.red_b("Sorry, that's not the correct input, please refer to the instruction
         for the input method"))
       take_input
     end
   end
 
-  def gen_secret_code
-    randomness = Random.new
-    @code = (1..4).map { |_| randomness.rand(1..6) }
-    puts(blue_b('The secret code has been generated, you may begin . . .'))
-  end
-
   def give_feedback(p_input = @guess); end
 
-  def evaluate_guess; end
-
   def validate_input(p_input)
-
     # note to self: imlpement map to replace
     p_input.each do |value|
-      # puts ("this is the val: #{value}")
       if /[1-6]/ === value.to_s
         # do nothing
       else
         return false
       end
     end
-  end
-
-  # checks if the game is over
-  # tells the user how many guesses they have left
-  def check_game_status;
-    if guesses_left <= 0
-      return 'game_over'
-    else
-      return 'game_continue'
-    end
-  end
-
-  # post-game prompts user whether they'd like to try again
-  def game_end
-    puts(cyan_b('Thanks for playing!'))
-    exit(0)
-  end
-
-  def game_over
-    puts('Nice try, but you ran our of guesses :(')
   end
 
   def prompt_start
@@ -128,40 +93,89 @@ class Game
     end
     return gets.chomp
   end
-
-  #colors
-  def colorize(text, color_code)
-    "\e[#{color_code}m#{text}\e[0m"
-  end
-
-  def red(text); colorize(text, 31); end
-  def red_b(text); colorize(text, 91); end
-  def yellow(text); colorize(text, 33); end
-  def yellow_b(text); colorize(text, 93); end
-  def green(text); colorize(text, 32); end
-  def green_b(text); colorize(text, 92); end
-  def blue(text); colorize(text, 34); end
-  def blue_b(text); colorize(text, 94); end
-  def purple(text); colorize(text, 35); end
-  def purple_b(text); colorize(text, 95); end
-  def cyan(text); colorize(text, 36); end
-  def cyan_b(text); colorize(text, 96); end
-  def white(text); colorize(text, 37); end
-  def white_b(text); colorize(text, 97); end
 end
 
-def main
-  the_game = Game.new
+# *****************************************************************************
 
-  if the_game.prompt_start.eql? 'y'
-    #game continues to begin
-  else
-    the_game.game_end
+class GameStatus
+
+  def check_game_status;
+    if guesses_left <= 0
+      return 'game_over'
+    else
+      return 'game_continue'
+    end
   end
 
-  the_game.introduce_user
-  the_game.gen_secret_code
-  the_game.take_input
+  def game_end
+    puts(Colors.cyan_b('Thanks for playing!'))
+    exit(0)
+  end
+
+  def game_over
+    puts('Nice try, but you ran our of guesses :(')
+  end
+end
+
+# *****************************************************************************
+
+class SecretCode
+  attr_reader :code
+
+  def initialize
+    @code = gen_secret_code
+  end
+
+  def gen_secret_code
+    randomness = Random.new
+    return (1..4).map { |_| randomness.rand(1..6) }
+    puts(blue_b('The secret code has been generated, you may begin . . .'))
+  end
+
+  def compare_to_secret_code; end
+end
+
+# *****************************************************************************
+
+class Colors
+  class << self
+    def red(text); colorize(text, 31); end
+    def red_b(text); colorize(text, 91); end
+    def yellow(text); colorize(text, 33); end
+    def yellow_b(text); colorize(text, 93); end
+    def green(text); colorize(text, 32); end
+    def green_b(text); colorize(text, 92); end
+    def blue(text); colorize(text, 34); end
+    def blue_b(text); colorize(text, 94); end
+    def purple(text); colorize(text, 35); end
+    def purple_b(text); colorize(text, 95); end
+    def cyan(text); colorize(text, 36); end
+    def cyan_b(text); colorize(text, 96); end
+    def white(text); colorize(text, 37); end
+    def white_b(text); colorize(text, 97); end
+
+    private
+
+    def colorize(text, color_code)
+      "\e[#{color_code}m#{text}\e[0m"
+    end
+  end
+end
+
+# *****************************************************************************
+
+def main
+  the_user = User.new
+
+  if the_user.prompt_start.eql? 'y'
+    #game continues to begin
+  else
+    the_user.game_end
+  end
+
+  the_user.introduce_user
+  the_secret_code = SecretCode.new
+  the_user.take_input
 end
 
 if __FILE__ == $0
