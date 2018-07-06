b70998c2b99932bff2007281ccc85d20229a9622
m-pitera <mpitera483@gmail.com>
2018-07-06 10:57:50 -0500

    set up skeleton for gen game type logic

--- lib/ai.rb
+++ lib/ai.rb
@@ -2,5 +2,9 @@ require './output'
 require './code'
 
 class AI
-
+  class << self
+    def solve
+      # AI uses it's high IQ to find the user-generated code within 5 guesses
+    end
+  end
 end
--- lib/input.rb
+++ lib/input.rb
@@ -13,6 +13,15 @@ class Input
         Code.compare(the_input)
       end
     end
+
+    def start_gen_type
+      prompt_secret_code
+      AI.solve
+    end
+
+    def start_guess_type
+
+    end
   end
 
     private
@@ -31,11 +40,19 @@ class Input
   def prompt_game_type
     Output.print_game_type_prompt
     if gets.chomp.downsize.eql? 'gen'
+      start_gen_type
       # user is prompted to generate code and the computer guesses
     elsif gets.chomp.downsize.eql? 'guess'
+      start_guess_type
       # user is given instructions
       # user is prompted for guess
       # ^ these two can be private methods that are called within a method called here
     end
+  end
+
+  def prompt_secret_code
+    Output.print_code_prompt
+    gets.chomp
+    # would send to Code class in order to assess whether valid
   end
 end
