15094ec7923af043f89ac47a047fcb4131f1c201
m-pitera <mpitera483@gmail.com>
2018-07-18 16:18:47 -0500

    calculating red and white pins for feedback

--- lib/secret_code.rb
+++ lib/secret_code.rb
@@ -14,5 +14,52 @@ class SecretCode
     return (1..4).map { |_| randomness.rand(1..6) }
   end
 
-  def compare_to_secret_code; end
+  # most of this is built to acompany the presence of a class that stores all data
+  # with that in mind there will be no params necessary as feedback and guess
+  # and all these vars will be referring to themselves as values within a hash
+  def compare_to_secret_code(guess)
+    if guess == code
+      red_pin = 4
+      return 'win'
+    else
+      feedback = Array.new(5) {'blank'}
+      feedback = check_for_red(guess, feedback)
+      feedback = check_for_white(guess, feedback)
+      count_pins(feedback)
+    end
+  end
+
+    private
+
+  def check_for_red(guess)
+    guess_index = 0
+
+    until guess_index == 4 do
+      feedback[guess_index] = 'red' if guess[guess_index] = code[guess_index]
+      guess_index += 1
+    end
+
+    return feedback
+  end
+
+  def check_for_white(guess)
+    guess_index = 0
+    code_index = 0
+
+    until guess_index == 4 do
+      until code_index == 4 do
+        next if feedback[code_index] != 'blank'
+        feedback[code_index] = 'white' if guess[guess_index] = code[code_index]
+      end
+    end
+
+    return feedback
+  end
+
+  def count_pins(feedback)
+    feedback.each do |pin|
+      red_pin += 1 if pin == 'red'
+      white_pin += 1 if pin == 'white'
+    end
+  end
 end
