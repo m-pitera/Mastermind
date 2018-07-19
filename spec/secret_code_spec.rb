11dcb53bff307351c67871192290834dc3171ccc
m-pitera <mpitera483@gmail.com>
2018-07-19 11:03:30 -0500

    created an outline for secret_code_spec file

---
+++ spec/secret_code_spec.rb
@@ -0,0 +1,26 @@
+require '../lib/secret_code'
+
+RSpec.describe SecretCode do
+  describe '#initialize' do
+    xit 'generates a secrect code when initialized' do
+
+    end
+  end
+  describe '#gen_secret_code' do
+    context 'creates a code that' do
+      xit 'is 4 characters' do
+
+      end
+      xit 'only contains numbers 1-6' do
+
+      end
+    end
+  end
+  describe '#compare_to_secret_code' do
+    context 'will correctly assign red and white pins' do
+      xit "WIP" do
+
+      end
+    end
+  end
+end
